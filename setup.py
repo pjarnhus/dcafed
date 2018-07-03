e1d40a1a506212eb44f66b48b973e854166cffc5
Philip R. Jarnhus <jarnhus@gmail.com>
2018-07-03 20:39:30 +0200

    Added content to setup file.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+
+setup(name='dcafed',
+      version='0.1',
+      description='A Data Science utility package for Data Cleaning, Analysis, Feature Engineering and Documentation',
+      author='Philip Roland Jarnhus',
+      author_email='jarnhus@gmail.com',
+      url='http://www.github.com/pjarnhus/dcafed',)
